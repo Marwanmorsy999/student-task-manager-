85f14908a1e8b29e3b67868b202451e957d6844e
ZiadMohamedHamada <ziadhamada25@gmail.com>
2026-05-16 09:55:58 +0300

    Add forgot password page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/layout/Navbar';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import DashboardPage from './pages/DashboardPage';
 import TasksPage from './pages/TasksPage';
 import CalendarPage from './pages/CalendarPage';
@@ -25,17 +26,92 @@ function AppRoutes() {
   return (
     <div className="min-h-screen bg-[var(--bg)]">
       <Navbar />
+
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/login"    element={<PublicRoute><LoginPage /></PublicRoute>} />
-          <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
-          <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-          <Route path="/tasks"     element={<PrivateRoute><TasksPage /></PrivateRoute>} />
-          <Route path="/calendar"  element={<PrivateRoute><CalendarPage /></PrivateRoute>} />
-          <Route path="/pomodoro"  element={<PrivateRoute><PomodoroPage /></PrivateRoute>} />
-          <Route path="/stats"     element={<PrivateRoute><StatsPage /></PrivateRoute>} />
-          <Route path="/settings"  element={<PrivateRoute><SettingsPage /></PrivateRoute>} />
+
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <RegisterPage />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/forgot-password"
+            element={
+              <PublicRoute>
+                <ForgotPasswordPage />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/tasks"
+            element={
+              <PrivateRoute>
+                <TasksPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/calendar"
+            element={
+              <PrivateRoute>
+                <CalendarPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/pomodoro"
+            element={
+              <PrivateRoute>
+                <PomodoroPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/stats"
+            element={
+              <PrivateRoute>
+                <StatsPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute>
+                <SettingsPage />
+              </PrivateRoute>
+            }
+          />
+
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
